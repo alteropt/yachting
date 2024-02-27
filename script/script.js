d2c5f0f5c3b85f9572ffa76db141e0d194748cdd
Vladislav <64088661+alteropt@users.noreply.github.com>
2024-02-27 14:44:31 +0300

    mobile version

--- script/script.js
+++ script/script.js
@@ -1,14 +1,17 @@
 window.onload = function() {
     // На случай большого количества колонок в блоке programm
     const programmEnums = document.querySelectorAll('.programm__enum')
-    programmEnums.forEach(programmEnum => {
-        if(programmEnum.classList.contains('route')) {
-            programmEnum.style.height = (programmEnum.offsetHeight / 1.8) + "px"
 
-        } else {
-            programmEnum.style.height = (programmEnum.offsetHeight / 2.8) + "px"
-        }
-    })
+    if(window.innerWidth > 768) {
+        programmEnums.forEach(programmEnum => {
+            if(programmEnum.classList.contains('route')) {
+                programmEnum.style.height = (programmEnum.offsetHeight / 1.8) + "px"
+                
+            } else {
+                programmEnum.style.height = (programmEnum.offsetHeight / 2.8) + "px"
+            }
+        })
+    }
 
     // FAQ
     const faqs = document.querySelectorAll('.faq')
@@ -20,7 +23,7 @@ window.onload = function() {
         })
     })
 
-    /* Burger */
+    // Burger
     const burgerOpen = document.querySelector('.burger')
     const burgerClose = document.querySelector('.burger-menu')
     const body = document.querySelector('body')
@@ -35,6 +38,16 @@ window.onload = function() {
         body.classList.remove('menu-opened')
         menu.classList.remove('active')
     })
+
+    // Article more
+    const more = document.querySelector('.start__more')
+    const hiddenText = document.querySelector('.start__hidden')
+
+    more.addEventListener('click', () => {
+        more.classList.add('active')
+        hiddenText.classList.add('active')
+    })
+
 }
 
 
